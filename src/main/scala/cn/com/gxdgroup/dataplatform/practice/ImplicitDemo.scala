4f5d3c59a1f85f193467664571bd01ef7913a61a
honest <honestold3@gmail.com>
2014-05-17 07:31:18 +0800

    add part of StorageLevel code!!

---
+++ src/main/scala/cn/com/gxdgroup/dataplatform/practice/ImplicitDemo.scala
@@ -0,0 +1,46 @@
+package cn.com.gxdgroup.dataplatform.practice
+
+/**
+ * Created by wq on 5/17/14.
+ */
+class ImplicitDemo {
+
+}
+
+class InsertA(insert : ImplicitDemo){
+
+  def add(msg : String){
+    println("msg:"+msg)
+  }
+
+}
+
+object test1 extends App{
+  implicit def kkk(insert : ImplicitDemo) = new InsertA(insert)
+  val test = new ImplicitDemo
+  test.add("wq")
+  //------------------------------------------------------
+
+  def testParam(str: String)(implicit name: String){
+    println("kankan:"+str+"::"+name)
+  }
+
+  implicit val name1 = "wq"
+
+  testParam("hehe")
+  testParam("aaa")("bbbk")
+
+  def test1(implicit str1: Int){
+    println(str1)
+  }
+  implicit val str1 = 1111
+  test1
+
+
+  //-------------------------------------------------------
+  implicit class Train(a: Int){
+    def add(a: Int) : Int  = a+a
+  }
+
+  println(2.add(2))
+}
