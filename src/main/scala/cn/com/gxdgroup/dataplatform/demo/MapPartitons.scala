3ed29141a20cce44d8ebad209cd3f3354e28311b
honest <honestold3@gmail.com>
2014-05-29 18:23:07 +0800

    test

--- src/main/scala/cn/com/gxdgroup/dataplatform/demo/MapPartitons.scala
+++ src/main/scala/cn/com/gxdgroup/dataplatform/demo/MapPartitons.scala
@@ -55,6 +55,61 @@ object MapPartitons {
 
     println("i:"+i)
     println(pp)
+
+    println("-----------------")
+    val l1 = List(1,2,3,4)
+    val l2 = List(5,6,7,8)
+    val d1 = sc.parallelize(l1)
+    val d2 = sc.parallelize(l2).cache()
+    val a1 =  d2.toArray()
+    val d3 = d1.mapPartitions{iter =>
+      iter.map{ x=>
+      //println("d2:"+d2.count())
+      println("a1:"+a1.size)
+      println("d1:"+x)
+      //x+d2.count()
+      }
+    }
+
+    d3.collect()
+
+    println("--------------------")
+
+    val list2 = List(("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),
+      ("a11",1),("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),("a11",1),
+      ("a11",1),("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),("a11",1),
+      ("a11",1),("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),("a11",1),
+      ("a11",1),("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),("a11",1),
+      ("a11",1),("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),("a11",1),
+      ("a11",1),("a1",1),("a2",1),("a3",1),("a4",1),("a5",1),("a6",1),("a8",1),("a9",1),("a10",1),("a11",1)
+      )
+    val td = sc.parallelize(list2,2)
+
+    val ccc = td.mapPartitions{iter =>
+
+      //println("ii:"+ii)
+      println("list2:"+list2.size)
+      iter.map{x=>
+        var ii =0
+        list2.map{y=>
+          ii+=x._2
+          //println(ii)
+        }
+        (x._1,ii)
+      }
+    }.collect().mkString("|")
+
+    println("td:"+ccc)
+
+
+    val ss= List(("7",1),("8",2),("8",3),("7",4),("8",5),("6",5),("6",9))
+
+    val ss1 = sc.parallelize(ss)
+
+    val ss2 = ss1.groupBy(x=>x._1)
+
+    println(ss2.collect().mkString("|"))
+
   }
 
 }
